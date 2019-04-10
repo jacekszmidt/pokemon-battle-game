2d7406e6fb83e617e7acada40b4f2e98c52803b4
jacekszmidt <github.jacek@gmail.com>
2019-04-10 20:39:49 +0200

    added: health bars, ESC to quit

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import pygame
 
 class Character:
     def __init__(self):
-        self.health = 100
+        self.health = 70
         self.spells = {"berserk": range(18, 25),
                        "fierce berserk": range(10, 35),
                        "light healing": range(12, 28)}
-        self.game = Game()
 
 
 class Bot(Character):
@@ -18,28 +17,51 @@ class Game:
     def __init__(self):
         pygame.init()
         self.stop_game = False
-        self.bg_color = [255, 255, 255]
+        self.bg_color = [211, 211, 211]
         self.display_width = 600
         self.display_height = 480
         self.score = 0
         self.fps_number = 15
+        self.clock = pygame.time.Clock()
         self.game_display = pygame.display.set_mode((self.display_width, self.display_height))
         pygame.display.set_caption('Pokemon_Battle_Game')
         self.character = Character()
+        self.character.health = 40
         self.bot = Bot()
-        self.health_green = pygame.Color(255, 160, 122, 255)
-        self.health_yellow = pygame.Color(255, 255, 153, 255)
-        self.health_red = pygame.Color(255, 0, 0, 255)
+        self.health_green = pygame.Color('green')
+        self.health_yellow = pygame.Color('yellow')
+        self.health_red = pygame.Color('red')
+        self.health_color_player = None  # https://stackoverflow.com/questions/40750584/this-inspection-detects-instance-attribute-definition-outside-init-method
+        self.health_color_bot = None
 
     def health_bar(self):
+        # hp bar for Character
         if self.character.health > 75:
-            self.health_color = self.health_green
+            self.health_color_player = self.health_green
         elif self.character.health > 50:
-            self.health_color = self.health_yellow
+            self.health_color_player = self.health_yellow
         else:
-            self.health_color = self.health_red
+            self.health_color_player = self.health_red
 
-        pygame.draw.rect(self.game_display, self.health_color, (580, 25, self.character.health, 25))
+        # hp bar for Bot
+        if self.bot.health > 75:
+            self.health_color_bot = self.health_green
+        elif self.bot.health > 50:
+            self.health_color_bot = self.health_yellow
+        else:
+            self.health_color_bot = self.health_red
+
+        pygame.draw.rect(self.game_display, self.health_color_player, [15, 5, self.character.health, 25])  # printing health bar
+        pygame.draw.rect(self.game_display, self.health_color_bot, [485, 5, self.bot.health, 25])  # printing health bar for Bot
+
+    # ESC to quit
+    def handle_keyboard_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.type == pygame.QUIT:
+                    self.stop_game = True
+                if event.key == pygame.K_ESCAPE:
+                    self.stop_game = True
 
     def game_over(self):
         if self.character.health <= 0 \
@@ -52,9 +74,11 @@ class Game:
     def game_loop(self):
         while not self.stop_game:
             self.game_display.fill(self.bg_color)
+            self.clock.tick(self.fps_number)
+            self.handle_keyboard_input()
+            self.health_bar()
             pygame.display.update()
             self.game_over()
-            self.health_bar()
 
 
 Game().game_loop()
